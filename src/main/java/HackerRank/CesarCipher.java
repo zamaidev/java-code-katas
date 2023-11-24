796be1d01fd6f8a34a8023578ce5e083754fd38c
antonioamaral <antonio.amaral@sensedia.com>
2023-11-24 16:52:09 -0300

    added new katas

---
+++ src/main/java/HackerRank/CesarCipher.java
@@ -0,0 +1,35 @@
+package HackerRank;
+
+import java.util.Arrays;
+
+public class CesarCipher {
+
+	public static String caesarCipher(String s, int k) {
+		String decoded = null;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '-') {
+				decoded = decode(s,k);
+			}
+		}
+		return decoded;
+	}
+
+	public static String decode(String s, int k) {
+		char[] decoded = s.toCharArray();
+		for (int i = 0; i < s.length(); i++) {
+			decoded[i+k] = s.charAt(i);
+			if (decoded[i+1] == null) {
+
+			}
+		}
+		return Arrays.toString(decoded);
+	}
+
+	public static void main(String args[]) {
+		String s = "middle-Outz";
+
+		String decoded = caesarCipher(s, 3);
+
+		System.out.println(decoded);
+	}
+}
---
+++ src/main/java/LeetCode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode () {}
+
+		TreeNode(int val) { this.val = val; }
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	List<Integer> list = new ArrayList<>();
+
+	public List<Integer> preorderTraversal(TreeNode root) {
+		traverseRead(root);
+		return list;
+	}
+
+	public void traverseRead(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+
+		list.add(root.val);
+		traverseRead(root.left);
+		traverseRead(root.right);
+	}
+}
+
