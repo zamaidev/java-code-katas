ba7df0b4867c32a8d778bb28db31e0be54312fae
antonioamaral <antonio.amaral@sensedia.com>
2023-12-02 17:13:35 -0300

    new katas

---
+++ src/main/java/LeetCode/Permutations.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Permutations {
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		backtrack(nums, 0, list);
+		return list;
+	}
+
+	public void backtrack (int[] nums, int index, List<List<Integer>> list) {
+		if (index == nums.length) {
+			List<Integer> ds = new ArrayList<>();
+			for (int i = 0; i < nums.length; i++) {
+				ds.add(nums[i]);
+			}
+			list.add(ds);
+			return;
+		}
+
+		for (int i = index; i < nums.length; i++) {
+			int temp = nums[i];
+			nums[i] = nums[index];
+			nums[index] = temp;
+
+			backtrack(nums, index+1, list);
+
+			temp = nums[i];
+			nums[i] = nums[index];
+			nums[index] = temp;
+		}
+	}
+}
