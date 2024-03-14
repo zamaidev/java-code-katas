8bcb9fec4fc9f51004d78f56aff21375dcd8fb4f
antonioamaral <antonio.amaral@sensedia.com>
2024-03-14 07:33:01 -0300

    updated kata

--- src/main/java/LeetCode/BestTimeToBuyAndSellStock.java
+++ src/main/java/LeetCode/BestTimeToBuyAndSellStock.java
@@ -8,36 +8,15 @@ public class BestTimeToBuyAndSellStock {
 	}
 
 	public static int maxProfit(int[] prices) {
-		// achar o menor. achar o maior
-		// se o maior vier antes do menor, retornar 0
-		// se nao, subtrair e retornar
-
-		int imenor = 0;
-		int imaior = 0;
-
-		int menor = prices[0];
-		for (int i = 0; i < prices.length; i++) {
-			if (prices[i] < menor) {
-				menor = prices[i];
-				imenor = i;
+		int buy = prices[0];
+		int profit = 0;
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] < buy) {
+				buy = prices[i];
+			} else if (prices[i] - buy > profit) {
+				profit = prices[i] - buy;
 			}
 		}
-		System.out.println("Menor "+menor+ " imenor: "+imenor);
-
-		int maior = prices[imenor];
-		for (int ii = imenor; ii < prices.length; ii++) {
-			if (prices[ii] > maior) {
-				maior = prices[ii];
-				imaior = ii;
-			}
-		}
-
-		System.out.println("maior "+maior+ " imaior: "+imaior);
-
-		if (imenor > imaior) {
-			return 0;
-		} else {
-			return prices[imaior]-prices[imenor];
-		}
+		return profit;
 	}
 }
