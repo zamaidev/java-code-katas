e30d4330a25f00b0334cfc4e01ffc814f04c50d9
Antonio Amaral <antonio.amaral2406@gmail.com>
2025-08-13 18:30:42 -0300

    test

--- src/main/java/leetcode/arrays/LengthOfLastWord.java
+++ src/main/java/leetcode/arrays/LengthOfLastWord.java
@@ -1,8 +1,5 @@
 package leetcode.arrays;
 
-import static leetcode.arrays.RemoveDuplicatesFromSortedArray.removeDuplicates;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
